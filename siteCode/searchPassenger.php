0dadffaa30700d914fd0c4e04162b178589c0d15
leo-m16 <leomatousian@gmail.com>
2017-12-06 00:29:30 +0000

    MYSQLI update
    Changing from MYSQL to MYSQLI

--- siteCode/searchPassenger.php
+++ siteCode/searchPassenger.php
@@ -9,7 +9,7 @@ if(isset($_POST['searchPassenger'] AND $_POST['Firstname'] AND $_POST['Surname']
 {
   $query="SELECT * FROM passenger_detail WHERE firstname=".$_POST['Firstname']."AND surname=".$_POST['Surname']."AND age=".$_POST['Age']."AND address=".$_POST['Address']."AND risk_type_id=".$_POST['RISK_TYPE_ID'];
   $passengers=mysqli_query($link,$query);
-  while ($row = mysqli_fetch_array($passengers,MYSQL_NUM)){
+  while ($row = mysqli_fetch_array($passengers,MYSQLI_NUM)){
     echo $row[0],$row[1],$row[2],$row[3],$row[4],$row[5];
 }
 
