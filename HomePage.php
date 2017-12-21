c45786683882e94b5c9e1b3b1774c5b797f989cf
Marina Sigona <33388548+MarinaSig@users.noreply.github.com>
2017-12-21 12:35:03 +0000

    Add files via upload

---
+++ HomePage.php
@@ -0,0 +1,67 @@
+
+<?php
+/*session_start();
+session_destroy();*/
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Passenger Targeting System</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Oswald|Raleway|Roboto|Source+Sans+Pro" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href= "css/HomePage.css">
+          </head>
+    <body>
+      <header>
+         <div class="container">
+                 <div id="branding">
+                     <h1><span class="highlight">The GreenLine</span> Passenger Targeting System </h1>
+                </div>
+
+                  <div>
+                     <nav>
+                        <ul>
+                      <li><a href="HomePage.php">Home</a></li>
+                      <li><a href="searchPassenger.php">Passenger Search</a></li>
+                      <li><a href="searchFlights.php">Flight Search</a></li>
+
+                   </ul>
+                       </nav>
+                   </div>
+               </div>
+            </header>
+
+
+         <section id="boxes">
+             <div class="wrapper">
+                  <br> <h3>The GreenLine System</h3>
+                  <br>
+                  <div class="container">
+                     <div class="container2">
+                         <p class="wrap">
+                             GreenLine Systems, provides its customers with the capability to improve decision-making in support of enforcement and facilitation mandates.
+                                      Started in 2002 by subject matter experts in border control, risk management, and logistics.
+GreenLine provides applications that assess the risk of vessels, cargo and travellers and provides technical consultants to both the U.S. Department
+of Homeland Security and Canada’s Canadian Border Security Agency.    With offices in Washington DC, Ottawa, and The Hague, we deliver risk management solutions
+for a broad spectrum of defence, intelligence, law, and regulatory enforcement agencies.  Our solutions help analysts and decision-makers address risks to safety,
+security, health and revenue. Today, our solutions are operational in the United States, Canada, Europe, Central America, South America, Africa, and the Caribbean
+supporting defence, anti-smuggling, and regulatory enforcement missions.
+</p><br>
+            <p align="center"><a href="searchPassenger.php"><button class="btnPassenger">Passenger Search</button></a> 
+              
+             <a href="searchFlights.php"><button class="btnFlight">Flight Search</button></a></p>
+              <br> </div>
+                   </div>
+               </div>
+       </section>
+
+
+    <footer class="mainFooter">
+        <p><a href="#" title="The GreenLine System" TheTheGreenLineSystem.com> GreenLine Systems </a>, Passenger Targeting System :: Copyright &copy; 2017</br>
+        Staffordshire University - MSc - Agile Software Development group project September - December 2017 </p>
+    </footer>
+
+   </body>
+
+
+</html>
