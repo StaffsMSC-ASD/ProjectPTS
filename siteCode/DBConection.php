643fb7e273d415247e56364eeb9bcbafa2c359f2
leo-m16 <leomatousian@gmail.com>
2017-12-27 23:02:22 +0000

    Deleting redundant pages

--- siteCode/DBConection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-     $conn = mysqli_connect('server','','','');
-//only use it for testing when go online remove it (sql injection Hacking)
-if (!$conn) {
-    die("Connection failed:" . mysqli_connect_error());
-}
-
-
